d9a5bfe3de5702b7d0ca3bf6ea7a88069a212ba8
LLBueno <leandro.bueno@fatec.sp.gov.br>
2019-11-08 08:36:22 -0300

    Coleta DEBUG das variaveis de ambiente

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b)lm6di8*7^!t2tygajmm!^o&r5fg(vsym$#hk=*@ew$w%zj*k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['*']
 
